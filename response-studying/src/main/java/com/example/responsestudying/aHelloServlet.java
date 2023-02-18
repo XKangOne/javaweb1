4ed0954fd1afff90ed9d143428d6065db0de49f5
yangkang <1910297447@qq.com>
2023-02-18 10:50:38 +0800

    response

--- response-studying/src/main/java/com/example/responsestudying/aHelloServlet.java
+++ response-studying/src/main/java/com/example/responsestudying/aHelloServlet.java
@@ -6,11 +6,11 @@ import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
-public class HelloServlet extends HttpServlet {
+public class aHelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World!";
+        message = "Heladadlo World!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
